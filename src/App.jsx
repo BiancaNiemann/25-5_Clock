b113b716996e620b0f9fb13b9c8b8b010ed6ce0c
Bianca Niemann <payrollsollutions@gmail.com>
2023-05-17 11:44:31 +0200

    26 tests passed (only auiod left)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-function App(){
+function App() {
 
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
@@ -8,39 +8,67 @@ function App(){
   const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(25)
   const [onBreak, setOnBreak] = useState(false)
+  const [sessionOrBreak, setSessionOrBreak] = useState(true)
+  const [startOrStop, setStartOrStop] = useState(false)
+  const [beep, setBeep] = useState(false)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if(isRunning){
-        if(seconds === 0){
+      if (isRunning) {
+        setBeep(false)
+        //seconds drop to nil, resets to 60 seconds and the subtracts on every second
+        if (seconds === 0) {
           setSeconds(60)
           setSeconds(prevSeconds => prevSeconds - 1)
-          if(minutes > 0){
+          //if seconds = nil and minutes are greater then nil then subtract one from minutes
+          if (minutes > 0) {
             setMinutes(prevMinutes => prevMinutes - 1)
-          } else {
+          //if seconds = nil
+          } else if (seconds === 0 && minutes === 0 ) {
+            setBeep(true)
             setMinutes(0)
             setSeconds(0)
-            //setIsRunning(false)
+            setIsRunning(false)
             setOnBreak(true)
+            setSessionOrBreak(false)
             setMinutes(breakLength)
-
-          }         
+          }
+        } else {
+          setSeconds(prevSeconds => prevSeconds - 1)
+        }
+      } else if (onBreak) {
+        setBeep(false)
+        if (seconds === 0) {
+          setSeconds(60)
+          setSeconds(prevSeconds => prevSeconds - 1)
+          if (minutes > 0) {
+            setMinutes(prevMinutes => prevMinutes - 1)
+          } else {
+            setBeep(true)
+            setMinutes(0)
+            setSeconds(0)
+            setIsRunning(true)
+            setOnBreak(false)
+            setSessionOrBreak(true)
+            setMinutes(sessionLength)
+          }
         } else {
           setSeconds(prevSeconds => prevSeconds - 1)
         }
       }
-      //
     }, 1000);
     return () => clearInterval(interval);
-  }, [isRunning, seconds]);
+  }, [isRunning, seconds, onBreak]);
 
-  function resetTimers(){
+  function resetTimers() {
     setMinutes(25)
     setSeconds(0)
     setBreakLength(5)
     setSessionLength(25)
     setIsRunning(false)
     setOnBreak(false)
+    setSessionOrBreak(true)
+    setStartOrStop(false)
   }
 
   function breakLengthTime(e) {
@@ -50,32 +78,38 @@ function App(){
       }
     } else if (e === '+' && !isRunning) {
       if (breakLength < 60) {
-      setBreakLength(prevBreak => prevBreak + 1)
+        setBreakLength(prevBreak => prevBreak + 1)
       }
     }
   }
 
   function sessionLengthTime(e) {
     if (e === '-') {
-      if (sessionLength > 1 && !isRunning) {
+      if (sessionLength > 1 && !isRunning && !onBreak) {
         setSessionLength(prevSession => prevSession - 1)
         setMinutes(prevMinutes => prevMinutes - 1)
       }
     } else if (e === '+') {
       if (sessionLength < 60 && !isRunning) {
-      setSessionLength(prevSession => prevSession + 1)
-      setMinutes(prevMinutes => prevMinutes + 1)
+        setSessionLength(prevSession => prevSession + 1)
+        setMinutes(prevMinutes => prevMinutes + 1)
       }
     }
   }
 
-  function startStop(){
-    setIsRunning(prevIsRunning => !prevIsRunning)
+  function startStop() {
+    if(sessionOrBreak){
+      setIsRunning(prevIsRunning => !prevIsRunning)
+    } else if(!sessionOrBreak){
+      setOnBreak(prevOnBreak => !prevOnBreak)
+    }
+
+    setStartOrStop(prevStartOrStop => !prevStartOrStop)
   }
 
 
-return (
-  <div>
+  return (
+    <div>
       <div id="break-label">Break Length</div>
       <button
         id="break-decrement"
@@ -106,14 +140,14 @@ return (
         Up
       </button>
 
-      <div id="timer-label">{onBreak ? "Break" : "Session"}</div>
+      <div id="timer-label">{!sessionOrBreak ? "Break" : "Session"}</div>
       <div id="time-left">{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</div>
 
       <button
         id="start_stop"
-        onClick={()=> startStop()}
+        onClick={() => startStop()}
       >
-        {!isRunning ? "Start" : "Stop"}
+        {!startOrStop  ? "Start" : "Stop"}
       </button>
       <button
         id="reset"
@@ -121,9 +155,9 @@ return (
       >
         Reset
       </button>
-      
+
     </div>
-)
+  )
 }
 
 export default App
