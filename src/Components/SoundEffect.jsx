45f5b2a53e4720cdc239e7463da6cd2edddf899a
Bianca Niemann <payrollsollutions@gmail.com>
2023-05-19 14:08:52 +0200

    fixed code on soundeffect

--- src/Components/SoundEffect.jsx
+++ src/Components/SoundEffect.jsx
@@ -5,7 +5,6 @@ export default function SoundEffect({audioRef }) {
     return (
         <div>
             <audio id="beep" src="https://www.pacdv.com/sounds/interface_sound_effects/sound10.mp3" ref={audioRef}></audio>
-            <h1>test</h1>
         </div>
 
     )
